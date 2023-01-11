03447b79ac0a859a34802e92840b5b4833247cc7
Doğa Oruç <aeris170@gmail.com>
2023-01-11 08:03:10 +0300

    Add support for AltGr character
    AltGr (or Alt+Ctrl under the hood) is used for some special characters in German/Turkish keyboards. These special characters include but are not limited to '[' ']' '{' '}'.

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -953,7 +953,7 @@ void TextEditor::HandleKeyboardInputs(bool aParentIsFocused)
 			EnterCharacter('\n', false);
 		else if (!IsReadOnly() && !alt && !ctrl && !super && ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Tab)))
 			EnterCharacter('\t', shift);
-		if (!IsReadOnly() && !io.InputQueueCharacters.empty() && !ctrl && !super)
+		if (!IsReadOnly() && !io.InputQueueCharacters.empty() && ctrl == alt && !super)
 		{
 			for (int i = 0; i < io.InputQueueCharacters.Size; i++)
 			{
