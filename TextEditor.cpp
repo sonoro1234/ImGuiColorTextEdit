0608a5c3e369253c7eb516aae769c5b0e54b97da
Santiago <san@san.san>
2023-11-02 23:20:01 +0100

    ensure both start and end of selection are visible when ctrl+d and find

--- TextEditor.cpp
+++ TextEditor.cpp
@@ -1145,7 +1145,7 @@ void TextEditor::SelectNextOccurrenceOf(const char* aText, int aTextSize, int aC
 	Coordinates nextStart, nextEnd;
 	FindNextOccurrence(aText, aTextSize, mState.mCursors[aCursor].mInteractiveEnd, nextStart, nextEnd, aCaseSensitive);
 	SetSelection(nextStart, nextEnd, aCursor);
-	EnsureCursorVisible(aCursor);
+	EnsureCursorVisible(aCursor, true);
 }
 
 void TextEditor::AddCursorForNextOccurrence(bool aCaseSensitive)
@@ -1163,7 +1163,7 @@ void TextEditor::AddCursorForNextOccurrence(bool aCaseSensitive)
 	SetSelection(nextStart, nextEnd, mState.mCurrentCursor);
 	mState.SortCursorsFromTopToBottom();
 	MergeCursorsIfPossible();
-	EnsureCursorVisible();
+	EnsureCursorVisible(-1, true);
 }
 
 bool TextEditor::FindNextOccurrence(const char* aText, int aTextSize, const Coordinates& aFrom, Coordinates& outStart, Coordinates& outEnd, bool aCaseSensitive)
@@ -1567,12 +1567,13 @@ float TextEditor::TextDistanceToLineStart(const Coordinates& aFrom, bool aSaniti
 		return aFrom.mColumn * mCharAdvance.x;
 }
 
-void TextEditor::EnsureCursorVisible(int aCursor)
+void TextEditor::EnsureCursorVisible(int aCursor, bool aStartToo)
 {
 	if (aCursor == -1)
 		aCursor = mState.GetLastAddedCursorIndex();
 
 	mEnsureCursorVisible = aCursor;
+	mEnsureCursorVisibleStartToo = aStartToo;
 	return;
 }
 
@@ -1601,12 +1602,12 @@ TextEditor::Coordinates TextEditor::SanitizeCoordinates(const Coordinates& aValu
 	}
 }
 
-TextEditor::Coordinates TextEditor::GetActualCursorCoordinates(int aCursor) const
+TextEditor::Coordinates TextEditor::GetActualCursorCoordinates(int aCursor, bool aStart) const
 {
 	if (aCursor == -1)
-		return SanitizeCoordinates(mState.mCursors[mState.mCurrentCursor].mInteractiveEnd);
+		return SanitizeCoordinates(aStart ? mState.mCursors[mState.mCurrentCursor].mInteractiveStart : mState.mCursors[mState.mCurrentCursor].mInteractiveEnd);
 	else
-		return SanitizeCoordinates(mState.mCursors[aCursor].mInteractiveEnd);
+		return SanitizeCoordinates(aStart ? mState.mCursors[aCursor].mInteractiveStart : mState.mCursors[aCursor].mInteractiveEnd);
 }
 
 TextEditor::Coordinates TextEditor::ScreenPosToCoordinates(const ImVec2& aPosition, bool aInsertionMode, bool* isOverLineNumber) const
@@ -2188,6 +2189,20 @@ void TextEditor::HandleMouseInputs()
 	}
 }
 
+void TextEditor::UpdateViewVariables(float aScrollX, float aScrollY)
+{
+	mContentHeight = ImGui::GetWindowHeight() - (IsHorizontalScrollbarVisible() ? IMGUI_SCROLLBAR_WIDTH : 0.0f);
+	mContentWidth = ImGui::GetWindowWidth() - (IsVerticalScrollbarVisible() ? IMGUI_SCROLLBAR_WIDTH : 0.0f);
+
+	mVisibleLineCount = Max((int)ceil(mContentHeight / mCharAdvance.y), 0);
+	mFirstVisibleLine = Max((int)(aScrollY / mCharAdvance.y), 0);
+	mLastVisibleLine = Max((int)((mContentHeight + aScrollY) / mCharAdvance.y), 0);
+
+	mVisibleColumnCount = Max((int)ceil((mContentWidth - Max(mTextStart - aScrollX, 0.0f)) / mCharAdvance.x), 0);
+	mFirstVisibleColumn = Max((int)(Max(aScrollX - mTextStart, 0.0f) / mCharAdvance.x), 0);
+	mLastVisibleColumn = Max((int)((mContentWidth + aScrollX - mTextStart) / mCharAdvance.x), 0);
+}
+
 void TextEditor::Render(bool aParentIsFocused)
 {
 	/* Compute mCharAdvance regarding to scaled font size (Ctrl + mouse wheel)*/
@@ -2207,16 +2222,7 @@ void TextEditor::Render(bool aParentIsFocused)
 	ImVec2 cursorScreenPos = ImGui::GetCursorScreenPos();
 	mScrollX = ImGui::GetScrollX();
 	mScrollY = ImGui::GetScrollY();
-	mContentHeight = ImGui::GetWindowHeight() - (IsHorizontalScrollbarVisible() ? IMGUI_SCROLLBAR_WIDTH : 0.0f);
-	mContentWidth = ImGui::GetWindowWidth() - (IsVerticalScrollbarVisible() ? IMGUI_SCROLLBAR_WIDTH : 0.0f);
-
-	mVisibleLineCount = Max((int)ceil(mContentHeight / mCharAdvance.y), 0);
-	mFirstVisibleLine = Max((int)(mScrollY / mCharAdvance.y), 0);
-	mLastVisibleLine = Max((int)((mContentHeight + mScrollY) / mCharAdvance.y), 0);
-
-	mVisibleColumnCount = Max((int)ceil((mContentWidth - Max(mTextStart - mScrollX, 0.0f)) / mCharAdvance.x), 0);
-	mFirstVisibleColumn = Max((int)(Max(mScrollX - mTextStart, 0.0f) / mCharAdvance.x), 0);
-	mLastVisibleColumn = Max((int)((mContentWidth + mScrollX - mTextStart) / mCharAdvance.x), 0);
+	UpdateViewVariables(mScrollX, mScrollY);
 
 	int maxColumnLimited = 0;
 	if (!mLines.empty())
@@ -2387,30 +2393,34 @@ void TextEditor::Render(bool aParentIsFocused)
 
 	if (mEnsureCursorVisible > -1)
 	{
-		auto pos = GetActualCursorCoordinates(mEnsureCursorVisible);
-		if (pos.mLine <= mFirstVisibleLine)
+		for (int i = 0; i < (mEnsureCursorVisibleStartToo ? 2 : 1); i++) // first pass for interactive end and second pass for interactive start
 		{
-			float targetScroll = std::max(0.0f, (pos.mLine - 0.5f) * mCharAdvance.y);
-			if (targetScroll < mScrollY)
-				ImGui::SetScrollY(targetScroll);
-		}
-		if (pos.mLine >= mLastVisibleLine)
-		{
-			float targetScroll = std::max(0.0f, (pos.mLine + 1.5f) * mCharAdvance.y - mContentHeight);
-			if (targetScroll > mScrollY)
-				ImGui::SetScrollY(targetScroll);
-		}
-		if (pos.mColumn <= mFirstVisibleColumn)
-		{
-			float targetScroll = std::max(0.0f, mTextStart + (pos.mColumn - 0.5f) * mCharAdvance.x);
-			if (targetScroll < mScrollX)
-				ImGui::SetScrollX(targetScroll);
-		}
-		if (pos.mColumn >= mLastVisibleColumn)
-		{
-			float targetScroll = std::max(0.0f, mTextStart + (pos.mColumn + 0.5f) * mCharAdvance.x - mContentWidth);
-			if (targetScroll > mScrollX)
-				ImGui::SetScrollX(targetScroll);
+			if (i) UpdateViewVariables(mScrollX, mScrollY); // second pass depends on changes made in first pass
+			Coordinates targetCoords = GetActualCursorCoordinates(mEnsureCursorVisible, i); // cursor selection end or start
+			if (targetCoords.mLine <= mFirstVisibleLine)
+			{
+				float targetScroll = std::max(0.0f, (targetCoords.mLine - 0.5f) * mCharAdvance.y);
+				if (targetScroll < mScrollY)
+					ImGui::SetScrollY(targetScroll);
+			}
+			if (targetCoords.mLine >= mLastVisibleLine)
+			{
+				float targetScroll = std::max(0.0f, (targetCoords.mLine + 1.5f) * mCharAdvance.y - mContentHeight);
+				if (targetScroll > mScrollY)
+					ImGui::SetScrollY(targetScroll);
+			}
+			if (targetCoords.mColumn <= mFirstVisibleColumn)
+			{
+				float targetScroll = std::max(0.0f, mTextStart + (targetCoords.mColumn - 0.5f) * mCharAdvance.x);
+				if (targetScroll < mScrollX)
+					ImGui::SetScrollX(mScrollX = targetScroll);
+			}
+			if (targetCoords.mColumn >= mLastVisibleColumn)
+			{
+				float targetScroll = std::max(0.0f, mTextStart + (targetCoords.mColumn + 0.5f) * mCharAdvance.x - mContentWidth);
+				if (targetScroll > mScrollX)
+					ImGui::SetScrollX(mScrollX = targetScroll);
+			}
 		}
 		mEnsureCursorVisible = -1;
 	}
--- TextEditor.h
+++ TextEditor.h
@@ -359,10 +359,10 @@ private:
 	void RemoveCurrentLines();
 
 	float TextDistanceToLineStart(const Coordinates& aFrom, bool aSanitizeCoords = true) const;
-	void EnsureCursorVisible(int aCursor = -1);
+	void EnsureCursorVisible(int aCursor = -1, bool aStartToo = false);
 
 	Coordinates SanitizeCoordinates(const Coordinates& aValue) const;
-	Coordinates GetActualCursorCoordinates(int aCursor = -1) const;
+	Coordinates GetActualCursorCoordinates(int aCursor = -1, bool aStart = false) const;
 	Coordinates ScreenPosToCoordinates(const ImVec2& aPosition, bool aInsertionMode = false, bool* isOverLineNumber = nullptr) const;
 	Coordinates FindWordStart(const Coordinates& aFrom) const;
 	Coordinates FindWordEnd(const Coordinates& aFrom) const;
@@ -385,6 +385,7 @@ private:
 
 	void HandleKeyboardInputs(bool aParentIsFocused = false);
 	void HandleMouseInputs();
+	void UpdateViewVariables(float aScrollX, float aScrollY);
 	void Render(bool aParentIsFocused = false);
 
 	void OnCursorPositionChanged();
@@ -412,6 +413,7 @@ private:
 	bool mShortTabs = false;
 
 	int mEnsureCursorVisible = -1;
+	bool mEnsureCursorVisibleStartToo = false;
 	bool mScrollToTop = false;
 
 	float mTextStart = 20.0f; // position (in pixels) where a code line starts relative to the left of the TextEditor.
